e0fba1dd91ca921709dda2a2e68bfad144c08516
Sophie Li <sophie.qx.li@gmail.com>
2025-05-04 00:11:02 -0700

    adaptive metro-hastings (its FIREEE)

--- metro_hastings.py
+++ metro_hastings.py
@@ -1,20 +1,43 @@
 import torch 
 import math
 
-#step 1: fixed proposal distribution (i.e. gaussian)
 def f_distr(x):
     #possible multivariate distributions
     #return torch.exp(-0.5 * torch.dot(x, x)) -> multivariate gaussian
     #return torch.exp(-torch.norm(x) ** 2)
     return torch.exp(-torch.norm(x/3))
 
+class MultiGaussian: 
+    #sophie's custom class
+    def __init__(self, mu: torch.Tensor, cov: torch.Tensor):
+        self.mu = mu
+        self.cov = cov
+        self.cov_inv = torch.linalg.inv(cov)
+        self.dim = mu.shape[0]
+        self.norm_const = torch.sqrt((2 * torch.pi) ** self.dim * torch.linalg.det(cov))
+        self.dist = torch.distributions.MultivariateNormal(mu, covariance_matrix=cov)
 
-def prop(mu, sigma= 0.5):
+    def pdf(self, x:torch.Tensor):
+        density = torch.exp(-0.5*(x-mu).T @ self.cov_inv @ (x-mu))
+        return density/self.norm_const
+
+    def sample(self):
+        return self.dist.sample()
+
+def prop(mu, cov = None, sigma="id"):
     #return torch.distributions.Normal(loc=mu, scale=sigma) single variable case 
-    cov = sigma**2 * torch.eye(len(mu))  # identity covariance
-    return torch.distributions.MultivariateNormal(loc=mu, covariance_matrix=cov)
+    if sigma == "id": 
+        sigma_val = 1.0
+        cov = sigma_val**2 * torch.eye(len(mu))  # identity covariance
+    if sigma == "prev_cov": 
+        assert cov is not None, "you have to provide a covariance matrix when using 'prev_cov'."
+        cov = cov
 
+    prop_gaussian = MultiGaussian(mu, cov)
+    return prop_gaussian
+    
 
+#fixed proposal distribution (i.e. gaussian)
 def metro_hastings_fixed(n:int, f, prop, x_samp): 
     x_samp.append(torch.zeros(n))
 
@@ -22,15 +45,15 @@ def metro_hastings_fixed(n:int, f, prop, x_samp):
     for i in range(its): 
         #proposed sample
         prev = x_samp[-1]
-        d = prop(mu = prev)
+        d = prop(mu = prev, sigma = "id")
         x_new = d.sample()
-        
 
-        d_old_given_new = prop(mu = x_new)
         #prob of proposing x_new given we're at prev
-        pdf_new_given_old = torch.exp(d.log_prob(torch.tensor(x_new)))
-        #prob of proposing prev given we're at x_new
-        pdf_old_given_new = torch.exp(d.log_prob(torch.tensor(prev)))
+        pdf_new_given_old = d.pdf(torch.tensor(x_new))
+
+        #prob of proposing prev given we're at x_new (reverse distribution)
+        d_old_given_new = prop(mu = x_new, sigma = "id")
+        pdf_old_given_new = d_old_given_new.pdf(torch.tensor(prev))
 
         p_accept = torch.min(torch.tensor(1.0), (f_distr(x_new)/f_distr(prev)) * (pdf_old_given_new / pdf_new_given_old))
         bern = torch.bernoulli(p_accept)
@@ -40,10 +63,41 @@ def metro_hastings_fixed(n:int, f, prop, x_samp):
         else: 
             x_samp.append(prev)
 
+def get_cov(X):
+    # each row is an observation, each col is a variable
+    X = torch.stack(X)
+    means = torch.mean(X, dim=0)
+    X = X - means
+    return torch.matmul(X.T, X) / (X.shape[0] - 1)
+
+
+#adaptive gaussian mixture method
+def MH_adaptive(n:int, f, prop, x_samp):
+    x_samp.append(torch.zeros(n))
+
+    its = 5000
+    for i in range(its): 
+        prev = x_samp[-1]
+
+        #the key difference: we use the existing cov-matrix!!
+        d = prop(mu = prev, cov = get_cov(x_samp), sigma = "prev_cov")
+        x_new = d.sample()
+
+        pdf_new_given_old = d.pdf(torch.tensor(x_new))
+        d_old_given_new = prop(mu = x_new, cov = get_cov(x_samp), sigma = "prev_cov")
+        pdf_old_given_new = d_old_given_new.pdf(torch.tensor(prev))
+
+        p_accept = torch.min(torch.tensor(1.0), (f_distr(x_new)/f_distr(prev)) * (pdf_old_given_new / pdf_new_given_old))
+        bern = torch.bernoulli(p_accept)
+        if bern == 1: x_samp.append(x_new)
+        else: x_samp.append(prev)
 
 x_samp = []
-n_dim = 2  # try 2D for visualization
-metro_hastings_fixed(n_dim, f_distr, prop, x_samp)
+n_dim = 2  
+#metro_hastings_fixed(n_dim, f_distr, prop, x_samp)
+
+#works WAYYY Better (try the visualization)
+MH_adaptive(n_dim, f_distr, prop, x_samp) 
 
 burn_in = 500
 samples = torch.stack(x_samp[burn_in:])
@@ -58,6 +112,4 @@ if n_dim == 2:
     plt.grid(True)
     plt.axis('equal')
     plt.show()
-
-
 
